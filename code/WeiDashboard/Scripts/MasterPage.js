f9f11b4ffd2a8b22e8c859392ca1f1a6e1c39ac4
muthukrishnan <48445878+firstmuthu@users.noreply.github.com>
2015-05-11 11:55:03 +0000

    Changes as on 2013-01-29.

--- code/WeiDashboard/Scripts/MasterPage.js
+++ code/WeiDashboard/Scripts/MasterPage.js
@@ -1,12 +1,12 @@
-﻿
-function SupportLinkButton_OnClientClick() {
-    var Page = "Support.aspx";
-    var Settings = "title.text:'Support';titlebar:0;center:yes;resizable:no;dialogWidth:345px;dialogHeight:210px;status:no;scroll:no;";
-    var objReturn = window.showModalDialog(Page, "", Settings);
-}
-
-function AboutLinkButton_OnClientClick() {
-    var Page = "About.aspx";
-    var Settings = "title.text:'About';titlebar:0;center:yes;resizable:no;dialogWidth:575px;dialogHeight:350px;status:no;scroll:no;";
-    var objReturn = window.showModalDialog(Page, "", Settings);
+﻿
+function SupportLinkButton_OnClientClick() {
+    var Page = "Support.aspx";
+    var Settings = "title.text:'Support';titlebar:0;center:yes;resizable:no;dialogWidth:345px;dialogHeight:210px;status:no;scroll:no;";
+    var objReturn = window.showModalDialog(Page, "", Settings);
+}
+
+function AboutLinkButton_OnClientClick() {
+    var Page = "About.aspx";
+    var Settings = "title.text:'About';titlebar:0;center:yes;resizable:no;dialogWidth:575px;dialogHeight:350px;status:no;scroll:no;";
+    var objReturn = window.showModalDialog(Page, "", Settings);
 }
